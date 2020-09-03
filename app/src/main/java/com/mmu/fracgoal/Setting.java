5f632f0a9e5149baf2ed5ac9956dda64c5e2b77a
Beckham Teoh <70683232+Shell1109@users.noreply.github.com>
2020-09-03 15:36:42 +0800

    Add files via upload

---
+++ app/src/main/java/com/mmu/fracgoal/Setting.java
@@ -0,0 +1,16 @@
+package com.mmu.fracgoal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Setting extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}

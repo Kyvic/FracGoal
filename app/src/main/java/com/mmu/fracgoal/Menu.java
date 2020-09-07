730ce5f25e5cc9522854aed34e2546ab5fdcfd6c
Vickey Tan <69897444+Kyvic@users.noreply.github.com>
2020-09-07 20:39:03 +0800

    Update Menu.java

--- app/src/main/java/com/mmu/fracgoal/Menu.java
+++ app/src/main/java/com/mmu/fracgoal/Menu.java
@@ -51,6 +51,7 @@ public class Menu extends AppCompatActivity {
         mSettingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Setting.class));
             }
         });
 
